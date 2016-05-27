29d7de744701b4f91bf025671694977318cb9e29
Stephen O'Kennedy <stephenoken@gmail.com>
2016-05-27 15:29:48 +0100

    "Can download images"
    ""

--- src/main/java/com/paser/ImageDownloader.java
+++ src/main/java/com/paser/ImageDownloader.java
@@ -5,6 +5,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,13 +17,18 @@ import java.util.List;
 
 public class ImageDownloader {
 
-
+    /*
+        Scrapes Website for images for pngs, gifs and jpegs
+        @returns : A list of image urls
+    */
     public List<String> getImgUrlsFromSite(String website) {
         ArrayList<String> imgUrls = new ArrayList();
         try{
             Document doc = Jsoup.connect(website).get();
             Elements imageTags = doc.getElementsByTag("img");
             for (Element e:imageTags) {
+                String url = e.attr("src");
+                if (isSupportedFormat(url))
                 imgUrls.add(e.attr("src"));
             }
         }catch (Exception e){
@@ -29,4 +36,36 @@ public class ImageDownloader {
         }
         return  imgUrls;
     }
+
+    private boolean isSupportedFormat(String url) {
+        return url.endsWith(".jpg") || url.endsWith(".JPEG") || url.endsWith(".png") || url.endsWith(".gif");
+    }
+
+    public void downloadsImages(List<String> imgUrlsFromSite, String destination){
+        try{
+            new File(destination+"/").mkdirs();
+            for (String link:imgUrlsFromSite) {
+                final URL url = new URL(link);
+                InputStream is = url.openStream();
+                OutputStream os = new FileOutputStream(this.getFileName(link));
+
+                byte[] b = new byte[2048];
+                int length;
+                while((length = is.read(b))!= -1){
+                    os.write(b,0,length);
+                }
+
+                is.close();
+                os.close();
+            }
+        } catch(IOException e){
+            System.err.println(e);
+        }
+
+    }
+
+    public String getFileName(String imgUrl){
+        return imgUrl.substring(imgUrl.lastIndexOf("/")+1,imgUrl.length());
+    }
+
 }
--- src/test/java/com/paser/ImageDownloaderTest.java
+++ src/test/java/com/paser/ImageDownloaderTest.java
@@ -2,6 +2,8 @@ package com.paser;
 
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,12 +11,33 @@ import static org.junit.Assert.*;
  */
 public class ImageDownloaderTest {
 
+    private String samplePage = "http://crosscountryrunning.tumblr.com/";
+    private String imgUrl = "http://66.media.tumblr.com/avatar_284cfbb01f43_128.png";
     @Test
     public void getImgUrlsFromSite(){
-        String samplePage = "http://crosscountryrunning.tumblr.com/";
-        String imgUrl = "http://66.media.tumblr.com/avatar_284cfbb01f43_128.png";
         ImageDownloader downloader = new ImageDownloader();
-        assertEquals(true,downloader.getImgUrlsFromSite(samplePage).contains(imgUrl));
-        assertEquals(true, downloader.getImgUrlsFromSite(null).isEmpty());
+        assertTrue(downloader.getImgUrlsFromSite(this.samplePage).contains(this.imgUrl));
+        assertTrue(downloader.getImgUrlsFromSite(null).isEmpty());
+    }
+
+    @Test
+    public void downloadImages(){
+        File f = new File("./test/crosscountryrunning/avatar_284cfbb01f43_128.png");
+        ImageDownloader downloader  = new ImageDownloader();
+        downloader.downloadsImages(downloader.getImgUrlsFromSite(this.samplePage),"test");
+        assertTrue(f.exists());
+    }
+
+    @Test
+    public void parseFileName(){
+        assertEquals("/avatar_284cfbb01f43_128.png",
+                new ImageDownloader().getFileName(this.imgUrl));
+    }
+
+    @Test
+    public void createDirectory(){
+        String path1 = "./sample-dir/images";
+        File f1 = new File(path1);
+        assertTrue(f1.isDirectory());
     }
 }
