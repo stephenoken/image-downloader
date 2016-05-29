e2331b81280ff859eca07ab633684f35025db2b3
Stephen O'Kennedy <stephenoken@gmail.com>
2016-05-29 16:26:58 +0100

    "Added stopwatch"
    ""

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,10 @@
 package com;
 
 
+import com.google.common.base.Stopwatch;
 import com.paser.ImageDownloader;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by stephenokennedy on 27/05/2016.
@@ -12,6 +15,9 @@ public class Main {
         if (args.length != 2){
             System.err.println("Missing Arguments Url and directory");
         }
+        final Stopwatch stopwatch = Stopwatch.createStarted();
         ImageDownloader.downloadsImages(ImageDownloader.getImgUrlsFromSite(args[0]),args[1]);
+        final long elapsedMillis = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+        System.out.println("CPU time " + elapsedMillis +" milliseconds.");
     }
 }
