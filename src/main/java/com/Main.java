75d739af676c091ea3cf94f77e1fe51529e9b817
Stephen O'Kennedy <stephenoken@gmail.com>
2016-05-29 16:08:42 +0100

    "Cleaned up the main method"
    ""

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,14 +1,7 @@
 package com;
 
-import com.image.ImageProcessor;
+
 import com.paser.ImageDownloader;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
-
-import static com.paser.ImageDownloader.*;
 
 /**
  * Created by stephenokennedy on 27/05/2016.
@@ -20,19 +13,5 @@ public class Main {
             System.err.println("Missing Arguments Url and directory");
         }
         ImageDownloader.downloadsImages(ImageDownloader.getImgUrlsFromSite(args[0]),args[1]);
-        getFilesFromDirectory(args[1]);
-    }
-
-    public static void getFilesFromDirectory(String directory){
-        File folder = new File(directory);
-        File[] listOfFiles = folder.listFiles();
-        try {
-            for (File f:listOfFiles) {
-                if (f.exists()&& f.isFile());
-                ImageProcessor.generateScaledImages(f);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
